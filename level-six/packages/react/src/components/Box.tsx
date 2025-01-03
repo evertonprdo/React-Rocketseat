9122bfd5f2d40cc9733e292e5bc8e8521cfa8948
evertonprdo <ecpj.prado@gmail.com>
2025-01-03 18:25:47 -0300

    fix: Add ref to TextInput and adjust styles

--- level-six/packages/react/src/components/Box.tsx
+++ level-six/packages/react/src/components/Box.tsx
@@ -3,7 +3,7 @@ import { styled } from '../styles'
 import { ComponentProps } from '@stitches/react'
 
 const StitchesBox = styled('div', {
-  padding: '$4',
+  padding: '$6',
   borderRadius: '$md',
   backgroundColor: '$gray800',
   border: '1px solid $gray600',
--- level-six/packages/react/src/components/TextInput/index.tsx
+++ level-six/packages/react/src/components/TextInput/index.tsx
@@ -1,15 +1,18 @@
-import { ComponentProps } from 'react'
+import { ComponentProps, ElementRef, forwardRef } from 'react'
 import { Input, Prefix, TextInputContainer } from './styles'
 
 export interface TextInputProps extends ComponentProps<typeof Input> {
   prefix?: string
+  cSize?: 'md' | 'sm'
 }
 
-export function TextInput({ prefix, ...props }: TextInputProps) {
-  return (
-    <TextInputContainer>
-      {!!prefix && <Prefix>{prefix}</Prefix>}
-      <Input {...props} />
-    </TextInputContainer>
-  )
-}
+export const TextInput = forwardRef<ElementRef<typeof Input>, TextInputProps>(
+  ({ prefix, cSize, ...props }: TextInputProps, ref) => {
+    return (
+      <TextInputContainer cSize={cSize}>
+        {!!prefix && <Prefix>{prefix}</Prefix>}
+        <Input ref={ref} {...props} />
+      </TextInputContainer>
+    )
+  }
+)
